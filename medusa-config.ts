cf1a829c087fb7022e6cbaec52b0020d5f19679e
hamzarashid <hamzarashid84@gmail.com>
2025-07-27 01:07:45 +0500

    db url fixed

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,7 +3,7 @@ import { loadEnv, defineConfig , Modules, ContainerRegistrationKeys } from '@med
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 
-const DATABASE_URL = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const DATABASE_URL = `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?sslmode=require`
 
 export default defineConfig({
   projectConfig: {
