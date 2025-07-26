5fe4c1614e18432e02718b695a2e3c27767831d3
hamzarashid <hamzarashid84@gmail.com>
2025-07-26 23:23:30 +0500

    DB addeed

--- medusa-config.ts
+++ medusa-config.ts
@@ -2,9 +2,20 @@ import { loadEnv, defineConfig , Modules, ContainerRegistrationKeys } from '@med
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
+
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_DATABASE = process.env.DB_DATABASE;
+
+const DATABASE_URL =
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
 export default defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl: DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
